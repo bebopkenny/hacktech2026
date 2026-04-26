b8132db41f1c3deb2f0cca3330a893d535819cd7
Anushna Gunda <gvsla.anu@gmail.com>
2026-04-25 18:25:24 -0700

    test

--- revit-plugin/config.py
+++ revit-plugin/config.py
@@ -5,3 +5,10 @@ WS_URL = os.getenv("REVITVYNC_WS_URL", "ws://107.191.50.160:8000/ws")
 
 # Unique identifier for this Revit session / user. Set per machine.
 SESSION_ID = os.getenv("REVITVYNC_SESSION_ID", "user-1")
+
+
+from pyrevit import forms
+from Autodesk.Revit.UI import TaskDialog
+
+forms.alert("Hello World from pyRevit!", title="pyRevit Demo")
+TaskDialog.Show("Hello World", "This is a Revit-native message.")
